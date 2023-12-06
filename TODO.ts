7ab8ac2249210abc520e4606cb1e774780c19900
copyandpaetow <lucas.paetow@gmail.com>
2023-12-06 17:33:07 +0100

    feat: initial base layout

--- TODO.ts
+++ TODO.ts
@@ -1,11 +1,20 @@
 /*
 TODO:
 
-- handle a11y
-=> reduce motion speed
-=> make the faces tabable
-=> enable arrow navigation
 
+# books
 
+should have 3 content blocks
+- spline => an anchor link to site.com#book-title
+- front
+- back
+
+Every one of them uses one pseudo element to create the 3 missing sides
+=> We can use a gradient to make the lines
+
+a11y:
+- the title will be a link to the blog post
+- arrow navigation the splines, the active one should get a highlight
+- tab navigation should also highlight the spline
 
 */
