5ed77bb536ff668fbfbdd730201702a559c364a8
Lucas Paetow <lucas.paetow@gmail.com>
2025-03-01 16:01:52 +0100

    feat: add grundlage tape

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-	server: { host: true },
-	site: "https://copyandpaetow.github.io",
+  server: { host: true },
+  site: "https://copyandpaetow.github.io",
+  devToolbar: { enabled: false },
 });
